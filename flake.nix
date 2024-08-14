ddf69b807ab77dd8b4b9a22b3359a56c67ea2b15
Avimitin <dev@avimit.in>
2024-08-14 20:54:00 +0800

    [nix] initialize flake skeleton
    Signed-off-by: Avimitin <dev@avimit.in>

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Chisel Nix";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = inputs@{ self, nixpkgs, flake-utils }:
+    let
+      overlay = import ./nix/overlay.nix;
+    in
+    {
+      # System-independent attr
+      inherit inputs;
+      overlays.default = overlay;
+    } // flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { overlays = [ overlay ]; inherit system; };
+      in
+      {
+        formatter = pkgs.nixpkgs-fmt;
+        legacyPackages = pkgs;
+      });
+}
---
+++ nix/overlay.nix
@@ -0,0 +1,2 @@
+final: prev:
+{ }
