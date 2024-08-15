e944e5bff5bc1ded7e28a13378031d82fc3cea40
Avimitin <dev@avimit.in>
2024-08-15 17:32:14 +0800

    [chisel] move riscv-test to separate directory
    Signed-off-by: Avimitin <dev@avimit.in>

--- templates/chisel/nix/overlay.nix
+++ templates/chisel/nix/overlay.nix
@@ -9,35 +9,11 @@ final: prev:
   circt-full = final.callPackage ./pkgs/circt-full.nix { };
   add-determinism = final.callPackage ./pkgs/add-determinism { }; # faster strip-undetereminism
 
-  riscv-tests = final.pkgsCross.riscv32-embedded.stdenv.mkDerivation rec {
-    pname = "riscv-tests";
-    version = "7878085d2546af0eb7af72a1df00996d5d8c43fb";
-    src = final.fetchFromGitHub {
-      owner = "riscv-software-src";
-      repo = "riscv-tests";
-      rev = "${version}";
-      hash = "sha256-CruSrXVO5Qlk63HPBVbwzl/RdxAAl2bknWawDHJwEKY=";
-    };
+  riscv-tests = final.callPackage ./pkgs/riscv-tests.nix {
+    stdenv = final.pkgsCross.riscv32-embedded.stdenv;
 
-    postUnpack = ''
-      rm -rf $sourceRoot/env
-      cp -r ${../tests/riscv-test-env} $sourceRoot/env
-    '';
-
-    enableParallelBuilding = true;
-
-    configureFlags = [
-      # to match rocket-tools path
-      "--prefix=${placeholder "out"}/riscv32-unknown-elf"
-    ];
-    buildPhase = "make RISCV_PREFIX=riscv32-none-elf-";
-    installPhase = ''
-      runHook preInstall
-      make install
-      mkdir -p $out/debug/
-      cp debug/*.py $out/debug/
-      runHook postInstall
-    '';
+    # Uncomment and replace this to other env
+    # riscv-test-env = ./path/to/riscv-test-env;
   };
 
   nvfetcherSource = final.callPackage ./pkgs/nvfetcher-source.nix { };
