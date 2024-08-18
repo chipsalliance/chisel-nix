1fcd49c43c30e8b53d355d666231ea8c1c1c114a
Jiuyang Liu <liu@jiuyang.me>
2024-08-18 19:54:05 +0800

    [gcd] rewrite elaborator

---
+++ templates/chisel/elaborator/src/GCD.scala
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2024 Jiuyang Liu <liu@jiuyang.me>
+package org.chipsalliance.t1.elaborator.rocketv
+
+import mainargs._
+import org.chipsalliance.gcd.{GCD, GCDParameter}
+import org.chipsalliance.t1.elaborator.Elaborator
+
+object GCD extends Elaborator {
+  @main
+  case class GCDParameterMain(
+      @arg(name = "xLen") xLen: Int,
+      @arg(name = "useAsyncReset") useAsyncReset: Boolean
+  ) {
+    def convert: GCDParameter = GCDParameter(xLen, useAsyncReset)
+  }
+
+  implicit def GCDParameterMainParser: ParserForClass[GCDParameterMain] =
+    ParserForClass[GCDParameterMain]
+
+  @main
+  def config(@arg(name = "parameter") parameter: GCDParameterMain) = configImpl(
+    parameter.convert
+  )
+
+  @main
+  def design(
+      @arg(name = "parameter") parameter: os.Path,
+      @arg(name = "run-firtool") runFirtool: mainargs.Flag,
+      @arg(name = "target-dir") targetDir: os.Path
+  ) =
+    designImpl[GCD, GCDParameter](parameter, runFirtool.value, targetDir)
+
+  def main(args: Array[String]): Unit = ParserForMethods(this).runOrExit(args)
+}
