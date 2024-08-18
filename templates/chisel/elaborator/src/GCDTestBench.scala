991fa81e4f361d5ff73eeda2b7cfa665b67120da
Jiuyang Liu <liu@jiuyang.me>
2024-08-18 19:54:05 +0800

    [gcd] rewrite elaborator

---
+++ templates/chisel/elaborator/src/GCDTestBench.scala
@@ -0,0 +1,76 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2024 Jiuyang Liu <liu@jiuyang.me>
+package org.chipsalliance.t1.elaborator.rocketv
+
+import mainargs._
+import org.chipsalliance.gcd.{
+  GCDTestBench,
+  GCDTestBenchParameter,
+  TestVerbatimParameter
+}
+import org.chipsalliance.t1.elaborator.Elaborator
+import org.chipsalliance.t1.elaborator.rocketv.GCD.GCDParameterMain
+
+object GCDTestBench extends Elaborator {
+  @main
+  case class GCDTestBenchParameterMain(
+      @arg(name =
+        "testVerbatimParameter"
+      ) testVerbatimParameter: TestVerbatimParameterMain,
+      @arg(name = "gcdParameter") gcdParameter: GCDParameterMain,
+      @arg(name = "timeout") timeout: Int,
+      @arg(name = "testSize") testSize: Int
+  ) {
+    def convert: GCDTestBenchParameter = GCDTestBenchParameter(
+      testVerbatimParameter.convert,
+      gcdParameter.convert,
+      timeout,
+      testSize
+    )
+  }
+
+  case class TestVerbatimParameterMain(
+      @arg(name = "useAsyncReset") useAsyncReset: Boolean,
+      @arg(name = "initFunctionName") initFunctionName: String,
+      @arg(name = "dumpFunctionName") dumpFunctionName: String,
+      @arg(name = "clockFlipTick") clockFlipTick: Int,
+      @arg(name = "resetFlipTick") resetFlipTick: Int
+  ) {
+    def convert: TestVerbatimParameter = TestVerbatimParameter(
+      useAsyncReset: Boolean,
+      initFunctionName: String,
+      dumpFunctionName: String,
+      clockFlipTick: Int,
+      resetFlipTick: Int
+    )
+  }
+
+  implicit def TestVerbatimParameterMainParser
+      : ParserForClass[TestVerbatimParameterMain] =
+    ParserForClass[TestVerbatimParameterMain]
+
+  implicit def GCDParameterMainParser: ParserForClass[GCDParameterMain] =
+    ParserForClass[GCDParameterMain]
+
+  implicit def GCDTestBenchParameterMainParser
+      : ParserForClass[GCDTestBenchParameterMain] =
+    ParserForClass[GCDTestBenchParameterMain]
+
+  @main
+  def config(@arg(name = "parameter") parameter: GCDTestBenchParameterMain) =
+    configImpl(parameter.convert)
+
+  @main
+  def design(
+      @arg(name = "parameter") parameter: os.Path,
+      @arg(name = "run-firtool") runFirtool: mainargs.Flag,
+      @arg(name = "target-dir") targetDir: os.Path
+  ) =
+    designImpl[GCDTestBench, GCDTestBenchParameter](
+      parameter,
+      runFirtool.value,
+      targetDir
+    )
+
+  def main(args: Array[String]): Unit = ParserForMethods(this).runOrExit(args)
+}
