d30d3e51d45b08eb2245695b4bb812f32a405837
unlsycn <unlsycn@unlsycn.com>
2024-09-11 11:13:54 +0800

    [gcdemu] implement plusarg to parse args
    Signed-off-by: unlsycn <unlsycn@unlsycn.com>

--- templates/chisel/nix/gcd/dpi-lib.nix
+++ templates/chisel/nix/gcd/dpi-lib.nix
@@ -14,7 +14,7 @@
 rustPlatform.buildRustPackage rec {
   name = "dpi-lib";
   src = ./../../${dpiLibName};
-  cargoHash = "sha256-soOva+RFpbA74zyoOIms0G6HLt7dpYcpatsBQSI3hsM=";
+  cargoHash = "sha256-DzedUaBSNOT552x+/4RjeUcKs68HxAiejQ60BgZnUYc=";
   buildFeatures = lib.optionals sv2023 [ "sv2023" ]
     ++ lib.optionals vpi [ "vpi" ] ++ lib.optionals enable-trace [ "trace" ];
 
