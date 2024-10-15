339886e4aaa234b1b816a6cad9b635736715b142
unlsycn <unlsycn@unlsycn.com>
2024-10-15 02:39:06 +0000

    [build] eliminate compilation warnings
    Signed-off-by: unlsycn <unlsycn@unlsycn.com>

--- templates/chisel/elaborator/src/GCDTestBench.scala
+++ templates/chisel/elaborator/src/GCDTestBench.scala
@@ -70,5 +70,5 @@ object GCDTestBenchMain extends SerializableModuleElaborator {
     os.write.over(targetDir / s"${topName}.anno.json", annos)
   }
 
-  def main(args: Array[String]): Unit = ParserForMethods(this).runOrExit(args)
+  def main(args: Array[String]): Unit = ParserForMethods(this).runOrExit(args.toIndexedSeq)
 }
