46999288edc2a302edfe95eebde61d3e66e5c037
Avimitin <dev@avimit.in>
2025-01-26 02:11:23 +0800

    [nix] fix overlay path

--- overlays/mill-flows.nix
+++ overlays/mill-flows.nix
@@ -1,6 +1,6 @@
 final: prev:
 {
-  configure-mill-home-hook = final.callPackage ./mill-flow/configure-mill-home.nix { };
-  fetchMillDeps = final.callPackage ./mill-flow/fetch-mill-deps.nix { inherit (final.xorg) lndir; };
-  publishMillJar = final.callPackage ./mill-flow/publish-mill-jar.nix { inherit (final.xorg) lndir; };
+  configure-mill-home-hook = final.callPackage ./mill-flows/configure-mill-home.nix { };
+  fetchMillDeps = final.callPackage ./mill-flows/fetch-mill-deps.nix { inherit (final.xorg) lndir; };
+  publishMillJar = final.callPackage ./mill-flows/publish-mill-jar.nix { inherit (final.xorg) lndir; };
 }
