6180c9a8a1e8c4b927c986a43288a47418c25129
Avimitin <dev@avimit.in>
2025-02-14 14:35:47 +0800

    [nix] replace fetchMillDeps with mill-ivy-fetcher
    Signed-off-by: Avimitin <dev@avimit.in>

--- overlays/mill-flows.nix
+++ overlays/mill-flows.nix
@@ -1,6 +1,18 @@
 final: prev:
 {
   configure-mill-home-hook = final.callPackage ./mill-flows/configure-mill-home.nix { };
-  fetchMillDeps = final.callPackage ./mill-flows/fetch-mill-deps.nix { inherit (final.xorg) lndir; };
   publishMillJar = final.callPackage ./mill-flows/publish-mill-jar.nix { inherit (final.xorg) lndir; };
+  mill-ivy-builder =
+    let
+      src = final.fetchFromGitHub {
+        owner = "Avimitin";
+        repo = "mill-ivy-builder";
+        rev = "e8451bd89494e6c5d23be52e39b515f4dec29edc";
+        hash = "";
+      };
+    in
+    {
+      generator = final.callPackage "${src}/package.nix" { };
+      dep-builder = final.callPackage "${src}/dep-builder.nix" { };
+    };
 }
