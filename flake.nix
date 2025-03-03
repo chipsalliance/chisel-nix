a3590b53941210e8339135296ae1d85dae9579bb
Avimitin <dev@avimit.in>
2025-03-04 01:09:21 +0800

    Ship mill-ivy-fetcher overlay
    Signed-off-by: Avimitin <dev@avimit.in>

--- flake.nix
+++ flake.nix
@@ -1,8 +1,23 @@
 {
   description = "Chisel Flakes";
 
-  outputs = { self }: {
+  inputs = {
+    mill-ivy-fetcher.url = "github:Avimitin/mill-ivy-fetcher";
+  };
+
+  outputs = { self, mill-ivy-fetcher }: {
     templates = import ./templates;
-    overlays.mill-flows = import ./overlays/mill-flows.nix;
+    overlays.mill-flows = final: prev:
+      let
+        overlays = [
+          (import ./overlays/mill-flows.nix)
+          (import mill-ivy-fetcher.overlays.default)
+        ];
+      in
+      prev.lib.foldl
+        (finalOverlay: overlayFn:
+          prev.lib.recursiveUpdate (overlayFn final prev) finalOverlay)
+        { } # fold init
+        overlays;
   };
 }
