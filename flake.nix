17d31497d2f6cbfc2d3d0ff6f23b62fdd47341e9
Avimitin <dev@avimit.in>
2025-03-18 00:37:00 +0800

    Add mill-ivy-fetcher
    Signed-off-by: Avimitin <dev@avimit.in>

--- flake.nix
+++ flake.nix
@@ -2,11 +2,12 @@
   description = "Chisel Flakes";
 
   inputs = {
-    mill-ivy-fetcher = "github:Avimitin/mill-ivy-fetcher";
+    mill-ivy-fetcher.url = "github:Avimitin/mill-ivy-fetcher";
   };
 
-  outputs = { self, mill-ivy-fetcher }: {
+  outputs = { self, mill-ivy-fetcher }@inputs: {
     templates = import ./templates;
-    overlays.mill-flows = mill-ivy-fetcher.default;
+    overlays.mill-flows = mill-ivy-fetcher.overlays.default;
+    inherit inputs;
   };
 }
