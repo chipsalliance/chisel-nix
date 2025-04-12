6f036b79d43269d6d5b1ddd1536b0d5de1d22f84
unlsycn <unlsycn@unlsycn.com>
2025-04-12 21:01:38 +0800

    [nix] migrate to flake-parts
    Signed-off-by: unlsycn <unlsycn@unlsycn.com>

--- templates/chisel/flake.nix
+++ templates/chisel/flake.nix
@@ -3,73 +3,82 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-    chisel-nix.url = "github:chipsalliance/chisel-nix";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     zaozi.url = "github:sequencer/zaozi";
     mill-ivy-fetcher.url = "github:Avimitin/mill-ivy-fetcher";
-    treefmt-nix.url = "github:numtide/treefmt-nix";
   };
 
   outputs =
     inputs@{
       self,
       nixpkgs,
-      flake-utils,
-      chisel-nix,
-      zaozi,
-      mill-ivy-fetcher,
-      treefmt-nix,
+      flake-parts,
+      ...
     }:
     let
       overlay = import ./nix/overlay.nix;
     in
-    {
-      # System-independent attr
-      inherit inputs;
-      overlays.default = overlay;
-    }
-    // flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = import nixpkgs {
-          # TODO: Do not depend on overlay of zaozi in favor of importing its outputs explicitly to avoid namespace pollution.
-          overlays = [
-            zaozi.overlays.default
-            mill-ivy-fetcher.overlays.default
-            (final: prev: { mill-ivy-fetcher = mill-ivy-fetcher.packages.${system}.default; })
-            overlay
-          ];
-          inherit system;
-        };
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
 
-        treefmtEval = treefmt-nix.lib.evalModule pkgs {
-          projectRootFile = "flake.nix";
-          programs.scalafmt = {
-            enable = true;
-            includes = [ "*.mill" ];
-          };
-          programs.nixfmt.enable = true;
-          programs.rustfmt.enable = true;
-        };
-      in
-      with pkgs;
-      {
-        formatter = treefmtEval.config.build.wrapper;
-        legacyPackages = pkgs;
-        devShells.default = mkShell (
-          {
-            inputsFrom = [ gcd.gcd-compiled ];
-            packages = [
-              cargo
-              rust-analyzer
-              nixd
-              nvfetcher
+      imports = [
+        inputs.treefmt-nix.flakeModule
+      ];
+
+      flake.overlays.default = overlay;
+
+      perSystem =
+        { system, pkgs, ... }:
+        {
+          _module.args.pkgs = import nixpkgs {
+            inherit system;
+            # TODO: Do not depend on overlay of zaozi in favor of importing its outputs explicitly to avoid namespace pollution.
+            overlays = with inputs; [
+              zaozi.overlays.default
+              mill-ivy-fetcher.overlays.default
+              overlay
             ];
-            RUST_SRC_PATH = "${rust.packages.stable.rustPlatform.rustLibSrc}";
-          }
-          // gcd.tb-dpi-lib.env
-          // gcd.gcd-compiled.env
-        );
-      }
-    );
+          };
+
+          legacyPackages = pkgs;
+
+          treefmt = {
+            projectRootFile = "flake.nix";
+            programs.scalafmt = {
+              enable = true;
+              includes = [ "*.mill" ];
+            };
+            programs.nixfmt.enable = true;
+            programs.rustfmt.enable = true;
+          };
+
+          devShells.default =
+            with pkgs;
+            mkShell (
+              {
+                inputsFrom = [ gcd.gcd-compiled ];
+                packages = [
+                  cargo
+                  rust-analyzer
+                  nixd
+                  nvfetcher
+                ];
+                RUST_SRC_PATH = "${rust.packages.stable.rustPlatform.rustLibSrc}";
+              }
+              // gcd.tb-dpi-lib.env
+              // gcd.gcd-compiled.env
+            );
+        };
+    };
 }
